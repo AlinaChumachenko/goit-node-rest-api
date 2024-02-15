4b252bcd9c210feb86621044e4591cafc691efd7
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2024-02-15 23:04:02 +0200

    start

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,128 @@
-import contactsService from "../services/contactsServices.js";
+import { Contact } from "../modals/contact.js";
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+  updateFavoriteSchema,
+} from "../schemas/contactsSchemas.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const { _id: owner } = req.user;
+    const { page = 1, limit = 10 } = req.query;
+    const skip = (page - 1) * limit;
+    const contacts = await Contact.find({ owner }, "-createdAt -uptatedAt", {
+      skip,
+      limit,
+    }).populate("owner", "email");
 
-export const getOneContact = (req, res) => {};
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { _id: owner } = req.user;
+    const contact = await Contact.findById(id).where("owner").equals(owner);
 
-export const createContact = (req, res) => {};
+    if (!contact) {
+      throw HttpError(404);
+    }
 
-export const updateContact = (req, res) => {};
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { _id: owner } = req.user;
+    const deletedContact = await Contact.findByIdAndDelete(id)
+      .where("owner")
+      .equals(owner);
+
+    if (!deletedContact) {
+      throw HttpError(404, "Not found");
+    }
+
+    res.status(200).json(deletedContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createContact = async (req, res, next) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { _id: owner } = req.user;
+    const newContact = await Contact.create({ ...req.body, owner });
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  try {
+    if (!Object.keys(req.body).length > 0) {
+      throw HttpError(400, "Body must have at least one field");
+    }
+    const { error } = updateContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+
+    const { id } = req.params;
+    const { _id: owner } = req.user;
+
+    const updatedContact = await Contact.findByIdAndUpdate(id, req.body, {
+      new: true,
+    })
+      .where("owner")
+      .equals(owner);
+    if (!updatedContact) {
+      throw HttpError(404, "Not found");
+    }
+
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateFavorite = async (req, res, next) => {
+  try {
+    if (!("favorite" in req.body)) {
+      throw HttpError(400, "Missing field favorite");
+    }
+
+    const { error } = updateFavoriteSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+
+    const { id } = req.params;
+    const { _id: owner } = req.user;
+
+    const updatedContact = await Contact.findByIdAndUpdate(id, req.body, {
+      new: true,
+    })
+      .where("owner")
+      .equals(owner);
+    if (!updatedContact) {
+      throw HttpError(404, "Not found");
+    }
+
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ schemas/usersSchemas.js
@@ -0,0 +1,14 @@
+import Joi from "joi";
+const emailRegexp =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export const registerSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+  subscription: Joi.string(),
+});
+
+export const loginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
